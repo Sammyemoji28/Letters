e1c3fc4001ec3769cce97abf803829a207270b65
Sammyemoji28 <samyra.sopory@gmail.com>
2024-07-21 10:04:58 -0700

    Add files via upload

---
+++ pangramLetters.py
@@ -0,0 +1,19 @@
+
+userSentence= input("Type a random sentence! ")
+
+letters = {'a':0,'b':0,'c':0,'d':0,'e':0,'f':0,'g':0,'h':0,'i':0,'j':0,'k':0,'l':0,'m':0,'n':0,'o':0,'p':0,'q':0,'r':0,'s':0,'t':0,'u':0,'v':0,'w':0,'x':0,'y':0,'z':0}
+
+for i in userSentence:
+    if i in letters:
+        letters[i]+=1
+
+pangram = True
+
+for j in letters.values():
+    if j == 0:
+        pangram = False
+
+if pangram == True:
+    print("Your sentence is a pangram! ")
+else:
+    print("Your sentence is not a pangram! ")
